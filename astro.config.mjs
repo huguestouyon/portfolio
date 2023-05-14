95944a82ca42672ac0c740df6aded7158507ba6f
huguestouyon <hugues.touyon@gmail.com>
2023-05-14 02:55:50 +0200

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.huguestouyon.github.io',
-    base: '/portfolio',
+    site: 'https://www.huguestouyon.online',
 });
